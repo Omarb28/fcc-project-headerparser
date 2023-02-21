341693c63becb1d31c61c6471e4eca7638690de5
Omarb28 <omarb28@gmail.com>
2023-02-22 02:14:09 +0300

    Add whoami endpoint

--- index.js
+++ index.js
@@ -19,9 +19,18 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+// whoami endpoint
+app.get('/api/whoami', function (req, res) {
+
+	let ipaddress = req.ip;
+	let language = req.headers['accept-language'];
+	let software = req.headers['user-agent'];
+	
+	res.json({
+		ipaddress: ipaddress,
+		language: language,
+		software: software
+	});
 });
 
 // listen for requests :)
